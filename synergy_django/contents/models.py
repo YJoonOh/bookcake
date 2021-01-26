3cb3cf1a8404a471c737ab18850b969a13ce6bb5
YJoonOh <yyjo1104@naver.com>
2021-01-26 10:06:52 +0900

    minor change

--- synergy_django/contents/models.py
+++ synergy_django/contents/models.py
@@ -20,15 +20,16 @@ class Book(models.Model):
 
 class Content(models.Model):
     objects = models.Manager()
+
     source = models.ForeignKey("Book", related_name='book', on_delete=models.CASCADE)
     category = models.ManyToManyField(Category)
     image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None, null=True, blank=True)
-    keyword = models.CharField(max_length = 30, blank=True, null=True)
-    key_line = models.CharField(max_length = 100, null=True)   ## All 페이지에 보여지는 핵심 문장
-    body = models.TextField(blank=True, null=True)
-    # page = models.CharField(max_length = 10, default="p.")
-    tmi = models.TextField(blank=True, null=True)
-    footnote = models.TextField(blank=True, null=True)
+    keywords = models.CharField(max_length = 100, blank=True, null=True)     ### 검색시 사용 (comma-separated)
+    title = models.CharField(max_length=30, blank=True, null=True)  ### 어드민 페이지에 보여질 오브젝트 이름
+    key_line = models.CharField(max_length = 100, null=True)   ### All 페이지에 보여지는 핵심 문장
+    body = models.TextField(blank=True, null=True)  ### 발췌 내용
+    tmi = models.TextField(blank=True, null=True)   ### 내 코멘트
+    footnote = models.TextField(blank=True, null=True)  ### 용어 설명
     created_at = models.DateField(auto_now_add=True, null=True)
 
     def __str__(self):
